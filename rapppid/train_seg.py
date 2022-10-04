d4edf3b5a1a0537989a15f1e699d5a4be051b65c
Joseph Szymborski <git@jszym.com>
2022-10-04 13:54:41 -0400

    Updated train_seq.py to have a CLI and to limit proteins to those in the training set.

--- rapppid/train_seg.py
+++ rapppid/train_seg.py
@@ -1,34 +1,53 @@
+import click
 import gzip
 import pickle
 import sentencepiece as spm
+import os
 
-spm.set_random_generator_seed(8675309)
 
-# Path to the seqs file
-SEQS_PATH = '../data/9606/uniref_sequences_rapppid_subset[rapppid_proteins_[common_string_9606.protein.links.detailed.v11.5_uniref.csv]_8675309.json].pkl.gz'
-VOCAB_SIZE = 250
+@click.command()
+@click.option('--seed', default=8675309, help='Random seed to use.')
+@click.option('--vocab_size', default=250, help='The size of the SentencePiece vocab.')
+@click.argument('seq_path')
+@click.argument('train_path')
+def seq_train(seed, vocab_size, seq_path, train_path):
+    spm.set_random_generator_seed(seed)
 
-with gzip.open(SEQS_PATH) as f:
-    seqs = pickle.load(f)
+    with gzip.open(train_path) as f:
+        pairs = pickle.load(f)
 
-aas = ['PAD', 'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'O', 'U']
+    proteins = set()
 
-print('making seqs file')
-with open('seqs.txt', 'w') as f:
-    for pid in seqs:
+    for pair in pairs:
+        p1, p2, y = pair
+        proteins.add(p1)
+        proteins.add(p2)
 
-        seq = seqs[pid]
-        seq = "".join([aas[r] for r in seq])
-        f.write(seq+'\n')
+    with gzip.open(seq_path) as f:
+        seqs = pickle.load(f)
 
-print('sentencepiece training')
+    aas = ['PAD', 'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V',
+           'O', 'U']
 
-spm.SentencePieceTrainer.train(input='seqs.txt', 
-model_prefix=f'smp{VOCAB_SIZE}', 
-vocab_size=VOCAB_SIZE,
-character_coverage=1.,
-bos_id=-1,
-eos_id=-1,
-pad_id=0,
-unk_id=1,
-)
+    with open('seqs.txt', 'w') as f:
+        for pid in seqs:
+            if pid in proteins:
+                seq = seqs[pid]
+                seq = "".join([aas[r] for r in seq])
+                f.write(seq + '\n')
+
+    spm.SentencePieceTrainer.train(input='seqs.txt',
+                                   model_prefix=f'smp{vocab_size}',
+                                   vocab_size=vocab_size,
+                                   character_coverage=1.,
+                                   bos_id=-1,
+                                   eos_id=-1,
+                                   pad_id=0,
+                                   unk_id=1,
+                                   )
+
+    os.unlink('seqs.txt')
+
+
+if __name__ == '__main__':
+    seq_train()
